f588337b325e04ed99d0798699240445007610f4
jeahyeok <jeahyeok1120@gmail.com>
2025-03-20 11:39:26 +0900

    v0.3 judge prime number

--- week03_prime.py
+++ week03_prime.py
@@ -1,12 +1,18 @@
+from keyboard import is_pressed
+
 number = int(input())
-count = 0
+is_prime = True
 
-for i in range(1, number+1):
-    if number % i == 0:
-        count = count + 1
+if number >= 2:
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
         print(i, end=" ")
+else:
+    is_prime = False
 
-if count == 2:
+# if count == 0:
+if is_prime:
     print(f"{number}는(은) 소수입니다.")
 else:
     print(f"{number}는(은) 소수가 아닙니다!.")
