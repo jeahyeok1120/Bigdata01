6ce8ccd649a00cb7caf232a83c555c20bf8a2a11
jeahyeok <jeahyeok1120@gmail.com>
2025-03-20 12:20:03 +0900

    v.0.4

---
+++ Bigdata/week03_prime.py
@@ -0,0 +1,21 @@
+from keyboard import is_pressed
+
+number = int(input())
+is_prime = True
+
+if number >= 2:
+    i = 2
+    while i * i <= number:
+   # for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+        print(i, end=" ")
+        i = i + 1
+else:
+    is_prime = False
+
+# if count == 0:
+if is_prime:
+    print(f"{number}는(은) 소수입니다.")
+else:
+    print(f"{number}는(은) 소수가 아닙니다!.")
