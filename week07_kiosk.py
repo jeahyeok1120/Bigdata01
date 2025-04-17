28177d2a19cb1b4414f9a0d27b40f629459c89c6
jeahyeok <jeahyeok1120@gmail.com>
2025-04-17 12:01:19 +0900

    v2.1 kiosk

--- week07_kiosk.py
+++ week07_kiosk.py
@@ -45,15 +45,21 @@ def print_receipt() -> None:
     print(f"총 주문 금액 : {total_price}원")
 
 while True:
-    menu = int(input(display_menu()))
-    if len(drinks) >= menu >= 1:
-        order_process(menu - 1)
+    try:
+        memu = 0
+        menu = int(input(display_menu()))
+        if len(drinks) >= menu >= 1:
+            order_process(menu - 1)
 
-    elif menu == len(drinks) + 1:
-        print("주문을 종료합니다")
-        break
-    else:
-        print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
+        elif menu == len(drinks) + 1:
+            print("주문을 종료합니다")
+            break
+        else:
+            print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
+    except ValueError:
+        print(f"문자를 입력할 수 없습니다. 숫자를 입력해주세요.")
+
+# except
+
 
 print_receipt()
-:
